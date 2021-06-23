ae915e104cba92d3883130b0d047d5e21db23aec
Spielberg Hanielly <spiielberg@gmail.com>
2021-06-23 13:58:31 -0300

    Configurando toasts

---
+++ src/config/toastConfig.ts
@@ -0,0 +1,11 @@
+import { DefaultToastOptions } from 'react-hot-toast/dist/core/types';
+
+export const toastConfig: DefaultToastOptions = {
+  position: 'top-center',
+  style: {
+    fontFamily: "'Roboto', sans-serif",
+    color: '#29292e',
+    background: '#fff',
+    boxShadow: '0 2px 12px rgba(0, 0, 0, 0.04)',
+  },
+};
