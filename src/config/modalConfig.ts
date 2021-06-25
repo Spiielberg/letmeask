d0e3bed2cb6468ce92ae05485ac5aa3e8f8c1a09
Spielberg Hanielly <spiielberg@gmail.com>
2021-06-25 11:51:44 -0300

    Adicionando arquivo de configuração do modal para não resetar estilização padrão

---
+++ src/config/modalConfig.ts
@@ -0,0 +1,10 @@
+import { Styles } from 'react-modal';
+
+export const modalConfig: Styles = {
+  overlay: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  },
+};
