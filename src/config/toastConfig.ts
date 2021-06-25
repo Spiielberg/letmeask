600e1415375b88c88142e70e72a4488156ed137a
Spielberg Hanielly <spiielberg@gmail.com>
2021-06-25 01:48:21 -0300

    Corrigindo estilização

--- src/config/toastConfig.ts
+++ src/config/toastConfig.ts
@@ -3,9 +3,11 @@ import { DefaultToastOptions } from 'react-hot-toast/dist/core/types';
 export const toastConfig: DefaultToastOptions = {
   position: 'top-center',
   style: {
+    position: 'relative',
+    top: 8,
     fontFamily: "'Roboto', sans-serif",
     color: '#29292e',
-    background: '#f8f8f8',
+    background: '#fefefe',
     boxShadow: '0 2px 12px rgba(0, 0, 0, 0.04)',
   },
 };
