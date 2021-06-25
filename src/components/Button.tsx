3e8cdcafac93d2313c4ef5e55653d7a99246e2a7
Spielberg Hanielly <spiielberg@gmail.com>
2021-06-24 21:41:25 -0300

    Adicionando opções de estilos do botão

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,8 +4,22 @@ import '../styles/button.scss';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   isOutlined?: boolean;
+  isCancelButton?: boolean;
+  isDangerButton?: boolean;
 };
 
-export function Button({ isOutlined = false, ...props }: ButtonProps) {
-  return <button className={`button ${isOutlined && 'outlined'}`} {...props} />;
+export function Button({
+  isOutlined = false,
+  isCancelButton = false,
+  isDangerButton = false,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={`button ${isOutlined && 'outlined'} ${
+        isCancelButton && 'cancel'
+      } ${isDangerButton && 'danger'}`}
+      {...props}
+    />
+  );
 }
