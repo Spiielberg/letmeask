523b9b4fdc925a4725e775070b9facce54345627
Spielberg Hanielly <spiielberg@gmail.com>
2021-06-25 13:35:59 -0300

    Configurando opções de destacar perguntas e marcar como respondidas

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
 import ReactModal from 'react-modal';
+import classNames from 'classnames';
 
 import { modalConfig } from '../config/modalConfig';
 import { toastConfig } from '../config/toastConfig';
@@ -46,6 +47,39 @@ export function AdminRoom() {
 
     setQuestionToDelete(undefined);
     toast.success('Pergunta excluída!', toastConfig);
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+      isHighlighted: false,
+    });
+  }
+
+  async function handleCheckQuestionAsNotAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: false,
+    });
+  }
+
+  function handleHighlightQuestion(questionId: string) {
+    questions.forEach(async question => {
+      if (question.id === questionId) {
+        await database.ref(`rooms/${roomId}/questions/${question.id}`).update({
+          isHighlighted: true,
+        });
+      } else if (question.isHighlighted) {
+        await database.ref(`rooms/${roomId}/questions/${question.id}`).update({
+          isHighlighted: false,
+        });
+      }
+    });
+  }
+
+  async function handleRemoveHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: false,
+    });
   }
 
   async function handleCloseRoom() {
@@ -94,7 +128,76 @@ export function AdminRoom() {
                   key={question.id}
                   content={question.content}
                   author={question.author}
+                  isAnswered={question.isAnswered}
+                  isHighlighted={question.isHighlighted}
                 >
+                  {!question.isAnswered && (
+                    <button
+                      className={classNames({
+                        highlighted: question.isHighlighted,
+                      })}
+                      type='button'
+                      onClick={
+                        question.isHighlighted
+                          ? () => handleRemoveHighlightQuestion(question.id)
+                          : () => handleHighlightQuestion(question.id)
+                      }
+                    >
+                      <svg
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          clipRule='evenodd'
+                          d='M12 17.9999H18C19.657 17.9999 21 16.6569 21 14.9999V6.99988C21 5.34288 19.657 3.99988 18 3.99988H6C4.343 3.99988 3 5.34288 3 6.99988V14.9999C3 16.6569 4.343 17.9999 6 17.9999H7.5V20.9999L12 17.9999Z'
+                          stroke='#737380'
+                          strokeWidth='1.5'
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                        />
+                      </svg>
+                    </button>
+                  )}
+                  <button
+                    className={classNames({
+                      answered: question.isAnswered,
+                    })}
+                    type='button'
+                    onClick={
+                      question.isAnswered
+                        ? () => handleCheckQuestionAsNotAnswered(question.id)
+                        : () => handleCheckQuestionAsAnswered(question.id)
+                    }
+                  >
+                    <svg
+                      width='24'
+                      height='24'
+                      viewBox='0 0 24 24'
+                      fill='none'
+                      xmlns='http://www.w3.org/2000/svg'
+                    >
+                      <circle
+                        cx='12.0003'
+                        cy='11.9998'
+                        r='9.00375'
+                        stroke='#737380'
+                        strokeWidth='1.5'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                      />
+                      <path
+                        d='M8.44287 12.3391L10.6108 14.507L10.5968 14.493L15.4878 9.60193'
+                        stroke='#737380'
+                        strokeWidth='1.5'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                      />
+                    </svg>
+                  </button>
                   <button
                     type='button'
                     onClick={() => setQuestionToDelete(question.id)}
